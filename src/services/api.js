8eaacaa36e2cb8d4f7ebd3f789b5bf294f25a949
Puneet Naik <naikpuneet09@gmail.com>
2026-06-08 21:11:25 +0530

    add url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://taskflow-backend-bk8r.onrender.com",
+  baseURL:  process.env.REACT_APP_API_URL,
 });
 
 API.interceptors.request.use((req) => {
